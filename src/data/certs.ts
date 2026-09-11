fa27c90f38389191854bd7986004c3c39f1b8327
kinetas <kimjh7302@naver.com>
2026-09-11 23:21:58 +0900

    정보처리기사 합격

--- src/data/certs.ts
+++ src/data/certs.ts
@@ -7,5 +7,5 @@ export interface Cert {
 export const certs: Cert[] = [
   { name: '1종보통운전면허증', org: '대구경찰청', date: '2020.01.22' },
   { name: 'SQLD', org: '한국데이터산업진흥원', date: '2026.03.27' },
-  { name: '정보처리기사', org: '한국산업인력공단', date: '2026.09.11(합격발표예정)' },
+  { name: '정보처리기사', org: '한국산업인력공단', date: '2026.09.11' },
 ]
